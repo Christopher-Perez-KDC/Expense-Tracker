1e4a270d7550fc601c0cc92f9d55ea40d63da042
Christopher Perez <christopher.perez19kdc@gmail.com>
2024-03-01 12:13:19 -0500

    Updated colors and text boxes and added new data to sql from webb

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NAaF1cXmhKYVtpR2Nbe05xdV9FY1ZTQ2YuP1ZhSXxXdkZjWH5fc3dXQWJfVEI=");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
